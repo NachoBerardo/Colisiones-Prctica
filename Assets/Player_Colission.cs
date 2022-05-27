1908efa8bd60d46559d50c6b5e5debc845332edd
NachoBerardo <101335543+NachoBerardo@users.noreply.github.com>
2022-05-27 09:39:43 -0300

    Initial commit

---
+++ Assets/Player_Colission.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Colission : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < 0)
+        {
+            transform.position = new Vector3(0, 3, 0);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Death Wall")
+        {
+            transform.position = new Vector3(0, 3, 0);
+        }
+    }
+}
---
+++ Assets/Test_Colision.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Colision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter()
+    {
+        Debug.Log("Contacto");
+    }
+
+    void OnCollisionExit()
+    {
+        Debug.Log("No contacto");
+    }
+
+    void OnCollisionStay()
+    {
+        Debug.Log("En contacto");
+    }
+
+    /* Primera coindicion: Que los elementos de la coision tenga un colider. Necesitamos un BoxColider que es parecido a una hitbox. El colider debe tener
+     una opcion que sea triger desactivado
+
+    Segunda condicion: Deben tener por lo menos un rigidbody. "Is Kinematic" debe estar descativado 
+
+    Tercera condicion: Debe haber una funcion on collision
+    */
+}
